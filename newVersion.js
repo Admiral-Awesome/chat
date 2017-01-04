52f019142e2c1d05f3dd13e4344c430aa7407608
Admiral-Awesome <orlovan17@gmail.com>
2017-01-04 19:55:38 +0200

    version generator finale version

--- newVersion.js
+++ newVersion.js
@@ -1,3 +1,6 @@
+//version generator 
+//like v0.04-4-Jan-2017.07-50-38
+
 var dateFormat = require('dateformat');
 var exec = require('child_process').exec;
    exec('git describe',
@@ -10,8 +13,11 @@ var exec = require('child_process').exec;
     	var now = new Date();
     	var ver = "v".concat(parseFloat(stdout.substr(1,stdout.indexOf("-")-1))+0.01).concat("-" + dateFormat(now, "d-mmm-yyyy.hh-MM-ss"));
     	 exec(`git tag -a ${ver} -m ${ver}`, function(err, out) {
-    	 		console.log(err)
-    	 		console.log(out)
+    	 		if (!err) {
+    	 			console.log("success")
+    	 			console.warn (`new version : ${ver}`)	
+    	 		}
+    	 		
     	 })
 
     })
