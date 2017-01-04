0c32a5d6bc2dc53aa3e7672f2ff9bae373215568
Admiral-Awesome <orlovan17@gmail.com>
2017-01-04 17:32:51 +0200

    login start

---
+++ api/controllers/UserController.js
@@ -0,0 +1,41 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `UserController.login()`
+   */
+  login: function (req, res) {
+    return res.json({
+      todo: 'login() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.logout()`
+   */
+  logout: function (req, res) {
+    return res.json({
+      todo: 'logout() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `UserController.signup()`
+   */
+  signup: function (req, res) {
+    return res.json({
+      todo: 'signup() is not implemented yet!'
+    });
+  }
+};
+
