68c0962cd5a560080af82f728ffa761f365a8fd5
Admiral-Awesome <orlovan17@gmail.com>
2017-01-05 10:27:43 +0200

    messages

---
+++ api/models/Messages.js
@@ -0,0 +1,17 @@
+/**
+ * Messages.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+  	text : { type : 'string'},
+  	author : { type : 'string'}
+
+
+  }
+};
+
