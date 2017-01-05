1452d029448370a124a4911ae2feb3ddd6dc64ef
Admiral-Awesome <orlovan17@gmail.com>
2017-01-05 21:42:17 +0200

    sockets start

--- api/controllers/MessagesController.js
+++ api/controllers/MessagesController.js
@@ -4,7 +4,17 @@
  * @description :: Server-side logic for managing messages
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
-
+var io =require('socket.io')(8000);
+io.on('connection', function(socket){
+	
+	socket.on('msg', function(id, msg) {
+		console.log(msg);
+	});
+  console.log("connected");
+  socket.on('disconnect', function(){
+  console.log("disconnected")
+   });
+});
 module.exports = {
 	
 };
