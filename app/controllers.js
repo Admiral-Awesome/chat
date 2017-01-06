5a0013a8d7e8143b16ba9351b718318176ffe7fe
Admiral-Awesome <orlovan17@gmail.com>
2017-01-06 11:32:01 +0200

    registration complete

--- app/controllers.js
+++ app/controllers.js
@@ -2,11 +2,30 @@ var app = angular.module('Chat');
 
 //to do 
 //realization of controllers
-app.controller('homeController', function($timeout,$scope,Version) {
+app.controller('homeController', function($scope,Version) {
 	Version._set($scope);
 })
-.controller('registrationController', function($scope) {
-
+.controller('registrationController', function($state,$scope, $http,$timeout) {
+	$scope.register = { 
+		login : "",
+		password : "",
+		name : "",
+		surname : ""
+	};
+	$scope.showSuccess = false;
+	$scope.registeration = function() {
+		$http.post(HOST_CONST + ":" + PORT_CONST+ "/user", $scope.register).then(function(resp) {
+			console.log(resp)
+			$scope.showSuccess = true;
+			$timeout (function() {
+				$scope.showSuccess = false;
+				$state.go("home")
+			},3000)
+		}, function(err) {
+		
+			alert("login is busy")
+		})
+	}
 })
 .controller('chatController', function($scope) {
 
