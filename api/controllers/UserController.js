02f760bd627aee78d257a24b6658c173b8ffc7ee
Admiral-Awesome <orlovan17@gmail.com>
2017-01-06 14:04:33 +0200

    crossdomain allowings

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -18,14 +18,20 @@ module.exports = {
    * `UserController.login()`
    */
  login: function (req, res) {
-   res.setHeader("Access-Control-Allow-Origin", "*");
+   // res.setHeader("Access-Control-Allow-Origin", "*");
+   
+res.header('Access-Control-Allow-Credentials', true);
+res.header('Access-Control-Allow-Origin', req.headers.origin);
+res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+
   User.find(req.body).exec(function(err, data) {
     if (data && data.length > 0) {
       var userData = req.body;
       delete userData.password;
       userData.authenticated = true;
       req.session.authenticated = userData;
-      return res.ok({msg  : 'Logged in successfully'})
+      return res.ok({msg  : 'Logged in successfully',res : req.session.authenticated})
     }
     return res.status(400).json({msg : "wrong login or password"});
   })
@@ -35,16 +41,26 @@ module.exports = {
   * status authentification
   */
   status : function(req, res) {
-     res.setHeader("Access-Control-Allow-Origin", "*");
+    console.log( req.session.authenticated)
+   res.header('Access-Control-Allow-Credentials', true);
+res.header('Access-Control-Allow-Origin', req.headers.origin);
+res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+
     if ( ! req.session.authenticated)
        req.session.authenticated = {authenticated : false};
+
     return res.status(200).json({status :  req.session.authenticated})
   },
   /**
    * `UserController.logout()`
    */
   logout: function (req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+res.header('Access-Control-Allow-Credentials', true);
+res.header('Access-Control-Allow-Origin', req.headers.origin);
+res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+
   req.session.authenticated = {authenticated : false}
   return res.json({msg : "logged out"})
 },
@@ -52,7 +68,7 @@ module.exports = {
 
    
   appVersion : function(req, res) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Origin", 'http://localhost:8080');
 
     res.status(200).json({version : VESION_APP.replace("\n", "")});
   },
--- config/cors.js
+++ config/cors.js
@@ -1,6 +1,6 @@
 module.exports.cors = {
    allRoutes: true,
-   origin: '*',
+   origin: 'http://localhost:8080',
    credentials: true,
    methods: 'GET, POST, PUT, DELETE, OPTIONS, HEAD',
    headers: 'content-type',
